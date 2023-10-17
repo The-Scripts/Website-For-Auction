5d431974c5a0aa44fe41dd50c34a599d7e0dcaff
Łukasz Stępień <lukaszatepien@gmail.com>
2023-10-17 15:15:15 +0200

    Add filters

---
+++ js/main.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Pobierz przyciski i elementy przedmiotów
+    const btnAll = document.getElementById("btnAll");
+    const btnBooks = document.getElementById("btnBooks");
+    const btnDecorations = document.getElementById("btnDecorations");
+    const items = document.querySelectorAll(".item");
+
+    // Dodaj obsługę kliknięcia przycisków
+    btnAll.addEventListener("click", function () {
+        showAllItems();
+    });
+
+    btnBooks.addEventListener("click", function () {
+        filterItems("ksiazka");
+    });
+
+    btnDecorations.addEventListener("click", function () {
+        filterItems("dekoracja");
+    });
+
+    // Funkcja pokazująca wszystkie przedmioty
+    function showAllItems() {
+        items.forEach((item) => {
+            item.style.display = "block";
+        });
+    }
+
+    // Funkcja filtrowania przedmiotów na podstawie kategorii
+    function filterItems(category) {
+        items.forEach((item) => {
+            const itemType = item.querySelector("img").alt.toLowerCase();
+            if (itemType.includes(category)) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        });
+    }
+});
