d90da609398ef696b39941a41fd9c9035e770ae3
kmarcinkowskij <km.dartmoor3@gmail.com>
2023-10-19 11:38:13 +0200

    made a few visual changes

--- js/scroll.js
+++ js/scroll.js
@@ -9,14 +9,14 @@ arrow.addEventListener('click', ()=> {
 function checkScroll() {
     if(window.scrollY >= 1200) {
         shown = true;
-        arrow.style.opacity = 1;
-        arrow.style.height = "70px";
+        arrow.style.opacity = "1";
+        arrow.style.height = "50px";
         arrow.style.transform = 'translateY(0%)';
         console.log('shown');
     } else {
         shown = false;
-        arrow.style.opacity = 0;
-        arrow.style.height = 0;
+        arrow.style.opacity = "0";
+        arrow.style.height = "0";
         arrow.style.transform = 'translateY(1000%)';
         console.log('not shown');
     }
