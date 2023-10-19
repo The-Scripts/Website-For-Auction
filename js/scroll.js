407010a0b16ab26b17d1c7cf8fc65e9a32dbfe13
Patryk-Ruzicki <arcysanta@gmail.com>
2023-10-19 11:21:47 +0100

    fix scroll

--- js/scroll.js
+++ js/scroll.js
@@ -10,11 +10,13 @@ function checkScroll() {
     if(window.scrollY >= 1200) {
         shown = true;
         arrow.style.opacity = 1;
+        arrow.style.height = "50px";
         arrow.style.transform = 'translateY(0%)';
         console.log('shown');
     } else {
         shown = false;
         arrow.style.opacity = 0;
+        arrow.style.height = 0;
         arrow.style.transform = 'translateY(1000%)';
         console.log('not shown');
     }
