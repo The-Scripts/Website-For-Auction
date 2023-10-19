474c3138ba2c6ffa845bf29a882a1b060f4a0b2b
Patryk-Ruzicki <arcysanta@gmail.com>
2023-10-19 11:11:53 +0100

    arrow up
    by Konrad

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+const arrow = document.querySelector('#up');
+const header = document.querySelector('.header');
+let shown = false;
+
+arrow.addEventListener('click', ()=> {
+    header.scrollIntoView({behavior: 'smooth'});    
+});
+
+function checkScroll() {
+    if(window.scrollY >= 1200) {
+        shown = true;
+        arrow.style.opacity = 1;
+        arrow.style.transform = 'translateY(0%)';
+        console.log('shown');
+    } else {
+        shown = false;
+        arrow.style.opacity = 0;
+        arrow.style.transform = 'translateY(1000%)';
+        console.log('not shown');
+    }
+}
+window.addEventListener('scroll', checkScroll);
